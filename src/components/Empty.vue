327ac496117925ea131eb3f5108a4c4d710b527b
student <student@GFL-1468.ad.geeksforless.net>
2017-10-30 20:48:36 +0200

    10302017-class

--- src/components/Empty.vue
+++ src/components/Empty.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="Empty">
     qweqwe
+    <table>
+      <thead>
+        <tr>
+          <td>‹</td>
+          <td  colspan="5">{{lang_month}}</td>
+          <td>›</td>
+        </tr>
+        <tr><td>Пн</td><td>Вт</td><td>Ср</td><td>Чт</td><td>Пт</td><td>Сб</td><td>Вс</td></tr>
+      </thead>
+      <tbody>
+        <!-- <tr v-for=""></tr> -->
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,8 +22,27 @@ export default {
   name: 'Empty',
   data () {
     return {
-
+      date: '',
+      lang_month: 'MONTH'
     }
+  },
+  methods:{
+    calendar: function (year, month){
+      var Dlast = new Date(year,month+1,0).getDate()
+      self.days = Array(+Dlast).fill('-');
+      var D = new Date(year,month,Dlast)
+      var DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay()
+      var DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay()
+
+      console.log(Dlast)
+      console.log(D)
+      console.log(DNlast)
+      console.log(DNfirst)
+      console.log(self.days)
+    }
+  },
+  created(){
+    this.calendar(new Date().getFullYear(), new Date().getMonth())
   }
 }
 </script>
